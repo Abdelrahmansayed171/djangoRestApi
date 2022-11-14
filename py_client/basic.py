2593b0307929ae944f715ff1018f14e3ca8c43bc
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2022-11-14 10:03:15 +0200

    basic API file created

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,4 +1,12 @@
 import requests
 
 # endpoint in term of API client is really a kind of URL 
-endpoint = ""
+endpoint = "https://httpbin.org/status/200"
+# /anything will echo what i send to it 
+endpoint = "https://httpbin.org/anything"
+
+requests.get() # a API method
+
+# Phone -> Camera -> APP -> API -> Camera (it's library API not REST APIs)
+
+# Rest APIs -> Web Api (Rest API work on the internet)
