77977fce607ece3a8643c16e17167d6ec081f57c
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2022-11-26 09:47:56 +0200

    Get Api from django project

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,9 +1,13 @@
+# There are more than one client
+# one from the web browser
+# one from python client
 import requests
 
 # endpoint in term of API client is really a kind of URL 
 endpoint = "https://httpbin.org/status/200/"
 # /anything will echo what i send to it 
 endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/" #"http://127.0.0.1:8000"
 
 get_response = requests.get(endpoint, data={"query" : "Hello world"}) # a API method (akenak fata7t el mawke3 mn el browser bs with code)
 # Every request will need a response to it so we will save 
@@ -11,7 +15,7 @@ get_response = requests.get(endpoint, data={"query" : "Hello world"}) # a API me
 # requests.get(endpoint, json={"query" : "Hello world"}) will be an json application
 # and message will be in data field not (form like data)
 print(get_response.text) #print raw text response
- 
+print(get_response.status_code)
 # Phone -> Camera -> APP -> API -> Camera (it's library API not REST APIs)
 
 # Rest APIs -> Web Api (Rest API work on the internet)
@@ -19,4 +23,4 @@ print(get_response.text) #print raw text response
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON (Javascript object notation)
-print(get_response.json()) # Format the JSON into it's form 
+# print(get_response.json()) # Format the JSON into it's form 
